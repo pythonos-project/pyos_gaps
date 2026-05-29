6610d218130945a7caf7f63ffc08b9cf00409c11
DomeniGeco <domenigeco@gmail.com>
2026-05-29 10:39:16 +0200

    Fix directory check to use os.path.is_dir

--- pyos_gaps/decorators.py
+++ pyos_gaps/decorators.py
@@ -10,7 +10,7 @@ def verify_pyos_dir_errorlevel():
     1 = cartella non esiste
     """
 
-    if is_dir("pythonos"):
+    if os.path.is_dir("pythonos"):
         return 0
     else:
         return 1
